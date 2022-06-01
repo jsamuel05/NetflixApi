0fce8791cd9ef7b6920c906b12fe2dfd503502e3
jsamuel05 <bombaatomica2134@gmail.com>
2022-06-01 17:31:27 +0100

    Starting the api

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv  = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users")
+
+dotenv.config()
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("DB connection is Sucess")
+}
+app.use(express.json())
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+ 
+
+app.listen(8800, ()=>{
+    console.log("backend is running")
+})
+
+
---
+++ routes/auth.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const router = express.Router()
+const User = require("../models/User")
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken")
+
+
+//REGISTER
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString()
+    })
+    try {
+        const user = await newUser.save();
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//LOGIN
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+        !user && res.status(401).json("Wrong password or username")
+
+        const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const originalPassowrd = bytes.toString(CryptoJS.enc.Utf8);
+
+        originalPassowrd !== req.body.password &&
+            res.status(401).json("Wrong password or username")
+
+            const acessToken = jwt.sign(
+                { id: user._id, isAdmin: user._isAdmin}, 
+                process.env.SECRET_KEY,
+                {expiresIn: "5d"}
+            )
+                
+        const {password, ...info} = user._doc;
+
+        res.status(200).json({...info, acessToken})
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+module.exports = router
