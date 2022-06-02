e7a30119d4fc14be750b64def048ebd33d9d4cf4
jsamuel05 <bombaatomica2134@gmail.com>
2022-06-02 17:18:24 +0100

    02/06

--- models/List.js
+++ models/List.js
@@ -4,8 +4,8 @@ const ListSchema = new mongoose.Schema(
     {
         title: { type: String, required: true, unique: true },
         type: { type: String },
-        genre: { type: Number },
-        context:{type:Array}
+        genre: { type: String },
+        content: {type:Array}
     },
     { timestamps: true }
 )
